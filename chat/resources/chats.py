e6f14194e125856507366112c9108d9951cb4d17
Tal Jacob <taljacob2@gmail.com>
2022-04-02 19:27:59 +0300

    concised `0001_initial` migration file
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- chat/resources/chats.py
+++ chat/resources/chats.py
@@ -1,5 +1,7 @@
 CHATS = [
     ['Bill Gates', 'Jane Doe'],
-    ['Bill Gates', 'Jane Doe'],
-    ['Bill Gates', 'Jane Doe'],
+    ['Bill Gates', 'Tim Cook'],
+    ['John Doe', 'Jane Doe'],
+    ['Bill Gates', 'John Doe', 'Jane Doe'],
+    ['Bill Gates', 'John Doe', 'Jane Doe', 'Tim Cook'],
 ]
